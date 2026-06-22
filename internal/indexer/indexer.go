e631895c3871a546473b467e9f3c5f667f2840c3
insizaki <zakialfsch@gmail.com>
2026-06-23 04:05:54 +0700

    setup indexer

--- internal/indexer/indexer.go
+++ internal/indexer/indexer.go
@@ -1,1 +1,49 @@
 package indexer
+
+import (
+	"database/sql"
+	"go/parser"
+	"sync"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/insizaki/unice-indexer/internal/db"
+)
+
+type Indexer struct {
+	client       *ethclient.Client
+	db           *db.Client
+	parser       *parser.Parser
+	watchList    []common.AddressLength
+	watchMu      sync.RWMutex
+	startBlock   uint64
+	batchSize    uint64
+	confirmDepth uint64
+	syncInterval time.Duration
+	factoryAddr  common.Address
+}
+
+func NewIndexer(
+	client *ethclient.Client,
+	database *sql.DB,
+	factoryAddr string,
+	startBlock, batchSize, confirmDepth uint64,
+	syncInterval time.Duration,
+) (*Indexer, error) {
+	factory := common.HexToAddress(factoryAddr)
+
+	idx := &Indexer{
+		client:       client,
+		db:           database,
+		factoryAddr:  factory,
+		startBlock:   startBlock,
+		batchSize:    batchSize,
+		confirmDepth: confirmDepth,
+		syncInterval: syncInterval,
+		watchList:    []common.Address{factory},
+	}
+
+	idx.parser = NewParser(idx)
+	return idx, nil
+}
